06fde6bc4b7604ae8065b1376b4f4be9e9e9c62a
Mark Bonicillo <markabonicillo@gmail.com>
2017-05-12 21:15:15 -0700

    Adding express dependency.

---
+++ server.js
@@ -0,0 +1,16 @@
+//using express with node js
+var express = require('express');
+
+////initialize app as an express application
+var app = express();
+
+//Server variables
+var ipaddress = '127.0.0.1';
+var port      = 3000;
+
+//calling some app methods
+app.use(express.static(__dirname+'/public'));
+app.listen(port, ipaddress);
+
+//do something
+console.log("hello world!");
