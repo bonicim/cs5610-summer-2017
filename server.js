282c31d24c18bf48589accaebcab96f070671010
Mark Bonicillo <markabonicillo@gmail.com>
2017-06-03 13:51:49 -0700

    Fixing server.js

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 //using express with node js
 var express = require('express');
 
-////initialize app as an express application
+//initialize app as an express application
 var app = express();
 
-//Server variables
-var ipaddress = '127.0.0.1'; // this is localhost
-var port      = 3000;
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname+'/'));
 
-//calling some app methods
-app.use(express.static(__dirname+'/public'));
-app.listen(port, ipaddress);
-
-//do something
-console.log("hello world!");
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
