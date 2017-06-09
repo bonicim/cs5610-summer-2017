841a1c19522d2f003986adf31f6d2dd714e59917
Mark Bonicillo <markabonicillo@gmail.com>
2017-06-08 19:12:50 -0700

    Started configuring Angular Routing. Config.js created but not complete. Index.html needs route directive.

---
+++ public/assignment/js/config.js
@@ -0,0 +1,11 @@
+(function() {
+    angular
+        .module("WebAppMaker")
+        .config(Config);
+    function Config($routeProvider) {
+        $routeProvider
+            .when("/login", {
+                templateUrl: "/views/user/login.view.client.html"
+            })
+    }
+})();
