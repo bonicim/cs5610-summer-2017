a43a70ae87dd91bebc547c15fe939fe227d9ff67
Mark Bonicillo <markabonicillo@gmail.com>
2017-06-15 11:42:42 -0700

    Adding api stubs and temp data objects.

--- public/assignment/services/page.service.client.js
+++ public/assignment/services/page.service.client.js
@@ -1,3 +1,46 @@
-/**
- * Created by bono on 6/14/17.
- */
+(function () {
+  angular
+    .module("WebAppMaker")
+    .factory("PageService", PageService);
+
+  function PageService() {
+    // temporary database
+    var pages = [{ "_id": "321", "name": "Post 1", "websiteId": "456", "description": "Lorem" },
+      { "_id": "432", "name": "Post 2", "websiteId": "456", "description": "Lorem" },
+      { "_id": "543", "name": "Post 3", "websiteId": "456", "description": "Lorem" }];
+
+
+    // api interface object
+    var api = {
+      "createPage" : createPage,
+      "findPageByWebsiteId" : findPageByWebsiteId,
+      "findPageById" : findPageById,
+      "updatePage" : updatePage,
+      "deletePage" : deletePage
+    };
+
+    return api;
+
+    function createPage(websiteId, page) {
+
+    }
+
+    function findPageByWebsiteId(websiteId) {
+
+    }
+
+    function findPageById(pageId) {
+
+    }
+
+    function updatePage(pageId, page) {
+
+    }
+
+    function deletePage(pageId) {
+
+    }
+
+  }
+
+})();
