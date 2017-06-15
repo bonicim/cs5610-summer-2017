2f78591cba6706e0ce2839abd479b65acee54a7f
Mark Bonicillo <markabonicillo@gmail.com>
2017-06-15 12:12:28 -0700

    Adding controller files and stubs.

--- public/assignment/views/user/user.controller.client.js
+++ public/assignment/views/user/user.controller.client.js
@@ -1,9 +1,42 @@
 (function() {
   angular
     .module("WebAppMaker")
-    .controller("LoginController", loginCtrl);
+    .controller("LoginController", LoginController)
+    .controller("RegisterController", RegisterController)
+    .controller("ProfileController", ProfileController);
 
-  function loginCtrl() {
+  function LoginController($location, UserService) {
+    var vm = this;
+    vm.login = login;
+    console.log("login check");
 
+    function login() {
+      // var user = null;
+      // user = UserService.findUserByCredential(vm.username, vm.password);
+      // if (user) {
+      //   //locationurl
+      // }
+      // else {
+      //   $location
+      // }
+      // // if user login successful
+      // $location.url("#!/user/" + user.id);
+      //
+      // //else
+      //
+      // $location("#!/register");
+    }
+
+
+  }
+
+  function RegisterController() {
+    var vm = this;
+    console.log("login register");
+  }
+
+  function ProfileController() {
+    var vm = this;
+    console.log("login profile");
   }
 })();
---
+++ public/assignment/views/website/website.controller.client.js
@@ -0,0 +1,20 @@
+(function() {
+  angular
+    .module("WebAppMaker")
+    .controller("WebsiteListController", WebsiteListController)
+    .controller("NewWebsiteController", NewWebsiteController)
+    .controller("EditWebsiteController", EditWebsiteController);
+
+    function WebsiteListController() {
+
+    }
+
+    function NewWebsiteController() {
+
+    }
+
+    function EditWebsiteController() {
+
+    }
+
+}());
