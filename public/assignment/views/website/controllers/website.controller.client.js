e3bd80ffcd05296c1912654410ddd6e5ae7fc6e5
Mark Bonicillo <markabonicillo@gmail.com>
2017-06-18 16:33:02 -0700

    Configured ng-routing for all hrefs.

--- public/assignment/views/website/controllers/website.controller.client.js
+++ public/assignment/views/website/controllers/website.controller.client.js
@@ -6,14 +6,25 @@
     .controller("EditWebsiteController", EditWebsiteController);
 
     function WebsiteListController($routeParams, $location, WebsiteService) {
+      // global vars
       var vm = this;
       vm.uid = $routeParams.uid;
       vm.websites = undefined;
 
+      // api's
+      vm.goToProfile = goToProfile;
+
+      // initializer
       function init() {
         vm.websites = WebsiteService.findWebsitesByUser(vm.uid);
       }
       init();
+
+      // implemented api's
+      function goToProfile() {
+        console.log(vm.uid);
+        $location.url("/user/" + vm.uid);
+      }
     }
 
     function NewWebsiteController() {
@@ -32,6 +43,10 @@
       // api's
       vm.updateWebsite = updateWebsite;
       vm.deleteWebsite = deleteWebsite;
+      vm.goToWebsiteList = goToWebsiteList;
+      vm.goToEditWebsite = goToEditWebsite;
+      vm.goToProfile = goToProfile;
+      vm.goToNewWebsite = goToNewWebsite;
 
       // initializes all uninitialized global variables
       init();
@@ -59,6 +74,19 @@
       // helpers
       function goToWebsiteList() {
         $location.url("/user/" + vm.uid + "/website");
+      }
+
+      function goToEditWebsite(uid, wid) {
+        $location.url("/user/" + uid + "/website/" + wid);
+      }
+
+      function goToProfile() {
+        console.log(vm.uid);
+        $location.url("/user/" + vm.uid);
+      }
+
+      function goToNewWebsite() {
+        $location.url("/user/" + vm.uid + "/website/new");
       }
 
     }
