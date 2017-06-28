333cb7e8500eadd18a2fede8a82f96f9bc890e86
Mark Bonicillo <markabonicillo@gmail.com>
2017-06-28 16:08:15 -0700

    Requiring specific server-side services.

--- assignment/app.js
+++ assignment/app.js
@@ -1,1 +1,4 @@
-console.log("Server side app");
+require('./services/user.service.server');
+require('./services/website.service.server');
+require('./services/page.service.server');
+require('./services/widget.service.server');
