ea17ac324366ecda03ce793b7fe74da35a76154e
Mark Bonicillo <markabonicillo@gmail.com>
2017-06-28 16:15:27 -0700

    Adding data table and one GET url.

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -1,3 +1,11 @@
-/**
- * Created by bono on 6/28/17.
- */
+// temporary database
+var users = [{_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+  {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+  {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+  {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }];
+
+app.get('/api/user', findAllUsers);
+
+function findAllUsers(req, res) {
+  res.send(users);
+}
