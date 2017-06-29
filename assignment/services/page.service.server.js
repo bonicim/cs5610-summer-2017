d52376354907b79e611199cf7ea6a312db034e3d
Mark Bonicillo <markabonicillo@gmail.com>
2017-06-28 17:18:30 -0700

    Adding app singleton and data tables to server service.

--- assignment/services/page.service.server.js
+++ assignment/services/page.service.server.js
@@ -1,3 +1,6 @@
-/**
- * Created by bono on 6/28/17.
- */
+var app = require('../../express');
+
+// temporary database
+var pages = [{ "_id": "321", "name": "Post 1", "wid": "456", "description": "Lorem" },
+  { "_id": "432", "name": "Post 2", "wid": "456", "description": "Lorem" },
+  { "_id": "543", "name": "Post 3", "wid": "456", "description": "Lorem" }];
--- assignment/services/website.service.server.js
+++ assignment/services/website.service.server.js
@@ -1,3 +1,9 @@
-/**
- * Created by bono on 6/28/17.
- */
+var app = require('../../express');
+
+var websites = [{ "_id": "123", "name": "Facebook",    "developerId": "456", "description": "Lorem" },
+  { "_id": "234", "name": "Tweeter",     "developerId": "456", "description": "Lorem" },
+  { "_id": "456", "name": "Gizmodo",     "developerId": "456", "description": "Lorem" },
+  { "_id": "890", "name": "Go",          "developerId": "123", "description": "Lorem" },
+  { "_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem" },
+  { "_id": "678", "name": "Checkers",    "developerId": "123", "description": "Lorem" },
+  { "_id": "789", "name": "Chess",       "developerId": "234", "description": "Lorem" }];
