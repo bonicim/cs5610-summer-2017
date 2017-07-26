b65248a1608de97c43c0c5298d0cbfb4ef3df112
Mark Bonicillo <markabonicillo@gmail.com>
2017-07-25 20:52:37 -0700

    Configuring server to use session, cookies, and passport.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // load libraries
 var app = require('./express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
 
 // server config
 app.set('port', (process.env.PORT || 5000));
@@ -12,6 +15,10 @@ app.listen(app.get('port'), function() {
 // body parser module
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cookieParser());
+app.use(session({ secret: process.env.SESSION_SECRET}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // server side web services and database services
 require('./assignment/app');
