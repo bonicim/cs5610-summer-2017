ed3407e737dfbc60893d182ab4847475f6de8783
Mark Bonicillo <markabonicillo@gmail.com>
2017-07-26 11:41:13 -0700

    Added passport-local. Reconfigured server.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
 
+
 // server config
 app.set('port', (process.env.PORT || 5000));
 app.use(app.express.static(__dirname+'/'));
@@ -12,15 +13,24 @@ app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
 
-// body parser module
+// configure libraries
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(cookieParser());
-app.use(session({ secret: process.env.SESSION_SECRET}));
-app.use(passport.initialize());
-app.use(passport.session());
+// configuration for cookies, session, passport must happen in that order; there are dependencies
+app.use(cookieParser()); //  Step 1: cookieParser MUST be created before session
+app.use(session({
+  secret: "foobar",
+  resave: true,
+  saveUninitialized: true
+})); // Step 2: 'secret' is required; used to sign & encrypt cookie; resave, saveUninitialized must be defined
+     //  process.env.SESSION_SECRET
+app.use(passport.initialize()); // Step 3:
+app.use(passport.session()); // Step 4:
+
+
 
 // server side web services and database services
-require('./assignment/app');
+//require('./assignment/app');
+require('./sandbox/session/session');
 
 
