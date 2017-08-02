14c365683f91dfe53e8fe9e3579f990c6f081b0d
Mark Bonicillo <markabonicillo@gmail.com>
2017-08-01 20:31:51 -0700

    Configuring db for project.

---
+++ project/model/model.server.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+mongoose.Promise = require('q').Promise;
+
+// local db connection
+// mongoose
+//   .connect('mongodb://localhost/webdev', {useMongoClient: true})
+//   .then(function () {
+//     console.log("success connecting to local mongo.")
+//   });
+//database config
+
+var connectionString = undefined;
+var connectionResponse = undefined;
+
+if (process.env.MONGODB_URI) {
+  connectionString = process.env.MONGODB_URI;
+  connectionResponse = "Connected to REMOTE mongodb: ";
+}
+else {
+  connectionResponse = "Failed to connect to REMOTE mongodb. ";
+}
+
+mongoose
+  .connect(connectionString, {useMongoClient: true})
+  .then(function() {
+    console.log(connectionResponse, connectionString);
+  })
+  .catch(function (err) {
+    console.log(connectionResponse, err);
+  });
